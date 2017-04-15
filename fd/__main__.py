6dd5434999937cf87cdca7ae6ce12fea46aa3c3f
nagracks <nagracks@users.noreply.github.com>
2017-04-15 16:15:28 +0530

    Assign args' dictionary value to variable

--- fd/__main__.py
+++ fd/__main__.py
@@ -10,16 +10,18 @@ from fd import (
         )
 
 technique = args['technique']
+path = args['path']
+
 if technique == 'n':
     print("File with same name")
-    for files_list in files_with_same_name(args['path']):
+    for files_list in files_with_same_name(path):
         duplicates_summary(files_list)
 
 elif technique == 's':
     print("Files with same size")
-    for files_list in files_with_same_size(args['path']):
+    for files_list in files_with_same_size(path):
         duplicates_summary(files_list)
 else:
     print("Files with same data")
-    for files_list in files_with_same_data(args['path']):
+    for files_list in files_with_same_data(path):
         duplicates_summary(files_list)
